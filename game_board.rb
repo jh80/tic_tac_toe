02eb07f6d7be0a4ab64c65e095dd16f42aeac7c7
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-09-22 19:25:08 -0700

    Put GameBoard class in it's own file

---
+++ game_board.rb
@@ -0,0 +1,24 @@
+class GameBoard
+  # TO DO come back later to tighten the creattion variable setting of squares (use each)
+  def initialize 
+    @a1 = " "
+    @a2 = " "
+    @a3 = " "
+    @b1 = " "
+    @b2 = " "
+    @b3 = " "
+    @c1 = " "
+    @c2 = " "
+    @c3 = " "
+  end
+
+  def print_board
+    # TO DO later refactor to use an each loop
+    puts "  1   2   3 "
+    puts "a  #{@a1} | #{@a2} | #{@a3} "
+    puts "  --- --- ---"
+    puts "b  #{@b1} | #{@b2} | #{@b3} "
+    puts "  --- --- ---"
+    puts "c  #{@c1} | #{@c2} | #{@c3} "
+  end
+end
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,28 +1,4 @@
-
-class GameBoard
-  # TO DO come back later to tighten the creattion variable setting of squares (use each)
-  def initialize 
-    @a1 = " "
-    @a2 = " "
-    @a3 = " "
-    @b1 = " "
-    @b2 = " "
-    @b3 = " "
-    @c1 = " "
-    @c2 = " "
-    @c3 = " "
-  end
-
-  def print_board
-    # TO DO later refactor to use an each loop
-    puts "  1   2   3 "
-    puts "a  #{@a1} | #{@a2} | #{@a3} "
-    puts "  --- --- ---"
-    puts "b  #{@b1} | #{@b2} | #{@b3} "
-    puts "  --- --- ---"
-    puts "c  #{@c1} | #{@c2} | #{@c3} "
-  end
-end
+require "./game_board.rb"
 
 class Player
   def initialize(name, mark)
@@ -31,13 +7,13 @@ class Player
   end
 
   def get_sq_choice
-    puts "#{@name.capitalize}, what sq do you pick?"
+    puts '#{@name.capitalize}, what sq do you pick?'
     sq_choice = gets.chomp
   end
 end
 
 game = GameBoard.new()
-player1 = Player.new("player1", "x")
-player2 = Player.new("player2", "o")
+player1 = Player.new('player1', 'x')
+player2 = Player.new('player2', 'o')
 game.print_board
 player1.get_sq_choice
