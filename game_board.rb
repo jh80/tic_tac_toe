f1cbc2789292e3cdb69e5e32422fe93723072965
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-09-23 12:56:34 -0700

    Create Square instances in initialize of GameBoard

--- game_board.rb
+++ game_board.rb
@@ -1,6 +1,14 @@
 class GameBoard
-  # TO DO come back later to tighten the creattion variable setting of squares (use each)
-  def initialize 
+
+  # TO DO come back later to tighten the creattion variable setting of squares (use each)def initialize(width, height=width) 
+    @squares = []
+    # Create all the necessary Square instances
+    (width * height).times do |sq_num|
+      # This works without rounding down because both numbers are integers
+      row_letter = ((sq_num/width) + 97).chr
+      col_num = (sq_num%width) + 1
+      @squares << Square.new(row_letter + (col_num.to_s))
+    end
     @a1 = " "
     @a2 = " "
     @a3 = " "
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,8 +1,9 @@
 require "./game_board.rb"
 require "./player.rb"
 
-game = GameBoard.new()
+game = GameBoard.new(2)
 player1 = Player.new('player1', 'x')
 player2 = Player.new('player2', 'o')
 game.print_board
-player1.get_sq_choice
+puts game.squares
+# player1.get_sq_choice
