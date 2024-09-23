9bb8f1848805fbf866a70f309888650eadc96236
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-09-22 19:05:41 -0700

    Add basic get_sq_choice method to Player class
    This method requests and gets a sq choice from the
    player. But it does not check for the sq the be a
    valid sq choice or available to the game board.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -24,5 +24,20 @@ class GameBoard
   end
 end
 
+class Player
+  def initialize(name, mark)
+    @name = name
+    @mark = mark
+  end
+
+  def get_sq_choice
+    puts "#{@name.capitalize}, what sq do you pick?"
+    sq_choice = gets.chomp
+  end
+end
+
 game = GameBoard.new()
+player1 = Player.new("player1", "x")
+player2 = Player.new("player2", "o")
 game.print_board
+player1.get_sq_choice
