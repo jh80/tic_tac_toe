610741dd70f5300f5fe8c96264e567f7e136812b
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-09-23 12:19:39 -0700

    Create Square class

---
+++ square.rb
@@ -0,0 +1,17 @@
+class Square 
+  @@all = []
+  @@top = "---"
+  @@bottom = @@top
+  @@r_side = "|"
+  @@l_side = @@r_side
+  def initialize(name)
+    @name = name
+    @default_contents = " "
+    @contents = @default_contents
+    @@all << self
+  end
+  
+  def available? 
+    @default_contents == @contents
+  end
+end
