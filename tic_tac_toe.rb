ee6a36a7b4008ac1e65b2f236e09695378e8faa1
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-09-23 17:33:26 -0700

    Draft game sequence

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -6,7 +6,20 @@ require "./square"
 game = GameBoard.new(3)
 player1 = Player.new('player1', 'x')
 player2 = Player.new('player2', 'o')
+
+
 game.print_board
-choice = player1.get_sq_choice 
-game.execute_pick(choice, player1)
+
+loop do
+  choice = player1.get_sq_choice 
+  break if game.execute_pick(choice, player1)
+end
+
+game.print_board
+
+loop do 
+  choice = player2.get_sq_choice
+  break if game.execute_pick(choice, player2)
+end
+
 game.print_board
