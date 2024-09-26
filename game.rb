226b822a1e1d01fb2d7a7b3fbee0e73223ad0464
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-09-26 14:12:02 -0700

    Move column/row/diagonal_winner? methods to Game class
    I originally created them in the GameBoard class.
    I also needed to create and attr_reader in GameBoard
    for the list of squares.

--- game.rb
+++ game.rb
@@ -1,7 +1,71 @@
 class Game
-  def initialize(width = 3, player1_name = "Player1", player2_name = "Player2")
+  # TO DO take out attr_accesssor if I can
+  attr_accessor :player1, :player2, :board
+
+  def initialize(width = 3, height = width, player1_name = "Player1", player2_name = "Player2")
     @player1 = Player.new(player1_name, "x")
     @player2 = Player.new(player2_name, "o")
+    @width =  width
+    @height = height
     @board = GameBoard.new(width)
+    
+  end
+
+  def column_winner?(player) 
+    # Start a check at top of each column
+    (0...@width).each do |column|
+      winner = true
+      # Check down the column
+      (column...(@width * @height)).step(@width) do |index|
+        unless @board.squares[index].contents == player.mark 
+          winner = false
+          break
+        end
+      end
+      if winner == true
+        return true 
+      end
+    end
+    return false
+  end 
+
+  # TO DO can row_winner? and column_winner? be reduced into one method?
+  def row_winner?(player) 
+    # Start at begining of each row
+    (0...(@width*@height)).step(@width) do |row|
+      winner = true 
+      # Check through row
+      (row...row+@width).each do |index| 
+        unless @board.squares[index].contents == player.mark 
+          winner = false
+          break
+        end 
+      end
+      if winner == true 
+        return true
+      end
+    end
+    return false
+  end
+
+  def diagonal_winner?(player) 
+    # Start at the top corners
+    (0...@width).step(@width - 1) do |starting_side|
+      winner = true 
+      # Check down diagonal
+      step_angle = starting_side == 0 ? (@width + 1) : (@width - 1)
+      ending_side = starting_side == 0 ? (@width * @height) : (@width * (@height - 1) + 1)
+      (starting_side...(ending_side)).step(step_angle) do |index| 
+        unless @board.squares[index].contents == player.mark 
+          winner = false
+          break 
+        end
+      end
+      # TO DO Shorten all these tiny coniditionals into one line
+      if winner == true 
+        return true
+      end
+    end
+    return false
   end
 end
--- game_board.rb
+++ game_board.rb
@@ -1,6 +1,7 @@
 require "./square.rb"
 
 class GameBoard
+  attr_reader :squares
   # TO DO come back later to tighten the creattion variable setting of squares (use each)
   def initialize(width, height=width) 
     @squares = []
@@ -59,63 +60,5 @@ class GameBoard
     else
       return false
     end
-  end
-
-  def column_winner?(player) 
-    # Start a check at top of each column
-    (0...@width).each do |column|
-      winner = true
-      # Check down the column
-      (column...(@width * @height)).step(@width) do |index|
-        unless @squares[index].contents == player.mark 
-          winner = false
-          break
-        end
-      end
-      if winner == true
-        return true 
-      end
-    end
-    return false
-  end 
-
-  # TO DO can row_winner? and column_winner? be reduced into one method?
-  def row_winner?(player) 
-    # Start at begining of each row
-    (0...(@width*@height)).step(@width) do |row|
-      winner = true 
-      # Check through row
-      (row...row+@width).each do |index| 
-        unless @squares[index].contents == player.mark 
-          winner = false
-          break
-        end 
-      end
-      if winner == true 
-        return true
-      end
-    end
-    return false
-  end
-
-  def diagonal_winner?(player) 
-    # Start at the top corners
-    (0...@width).step(@width - 1) do |starting_side|
-      winner = true 
-      # Check down diagonal
-      step_angle = starting_side == 0 ? (@width + 1) : (@width - 1)
-      ending_side = starting_side == 0 ? (@width * @height) : (@width * (@height - 1) + 1)
-      (starting_side...(ending_side)).step(step_angle) do |index| 
-        unless @squares[index].contents == player.mark 
-          winner = false
-          break 
-        end
-      end
-      # TO DO Shorten all these tiny coniditionals into one line
-      if winner == true 
-        return true
-      end
-    end
-    return false
   end
 end
