5d36137569d9f603be1eabdcedff184e874f67a2
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-09-30 08:39:03 -0700

    Create and run game
    I took out my sort of test version of running the
    program.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -9,10 +9,17 @@ require "./game.rb"
 
 our_game = Game.new()
 
-our_game.board.execute_pick("b2", our_game.player1)
-our_game.board.print_board
-puts our_game.board.sq_available?(our_game.board.find_sq_by_name("b2", our_game.board.squares))
-puts our_game.board.sq_available?(our_game.board.squares[0])
+# puts our_game.board.find_sq_by_name("b2").contents
+# puts our_game.board.find_sq_by_name("b2")
+# our_game.board.execute_pick("b2", our_game.player1)
+# puts our_game.board.find_sq_by_name("b2").contents
+# puts our_game.board.find_sq_by_name("b2")
 
-#our_game.play
+# our_game.board.execute_pick("b2", our_game.player1)
+# our_game.board.print_board
+# puts our_game.board.sq_available?(our_game.board.find_sq_by_name("b2", our_game.board.squares))
+# puts our_game.board.sq_available?(our_game.board.squares[0])
+
+our_game.play
+
 
