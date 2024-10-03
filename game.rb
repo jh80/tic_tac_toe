3fe0443158cdea886fb52d4a90e5c07a854e6294
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-10-03 11:20:09 -0700

    Create approve_dimension method for initialize

--- game.rb
+++ game.rb
@@ -17,21 +17,11 @@ class Game
   }
 
   def initialize(width = 3, height = width, player1_name = "Player1", player2_name = "Player2")
-    if width > 10 
-      width = 10
-      puts @@messages[:board_at_max_width]
-    end
-
-    if height > 10
-      height = 10
-      puts @@message[:board_at_max_height]
-    end
-
     @player1 = Player.new(player1_name, "x")
     @player2 = Player.new(player2_name, "o")
-    @width =  width
-    @height = height
-    @board = GameBoard.new(width)
+    @width = approve_dimention(width, 10)
+    @height = approve_dimention(height, 10)
+    @board = GameBoard.new(@width)
   end
 
   def column_winner?(player) 
@@ -130,5 +120,13 @@ class Game
       return true
     end
     cat_game?
+  end
+
+  # TO DO fix print message to correctly apply to dimension
+  def approve_dimention(dimension, dimension_max) 
+    if dimension > dimension_max 
+      puts @@messages[:board_at_max_width]
+      return dimension_max
+    end
   end
 end
